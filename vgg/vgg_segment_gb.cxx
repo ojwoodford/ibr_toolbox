836f8c86ed63c3daa0c21452bcc73d92374ac5f8
Oliver Woodford <oliver.woodford@snapchat.com>
2018-09-12 10:57:42 +0100

    Fix MSVC compile error

--- vgg/vgg_segment_gb.cxx
+++ vgg/vgg_segment_gb.cxx
@@ -5,14 +5,7 @@
 #include <mex.h>
 
 // Define types
-#ifdef _MSC_VER
-typedef __int8 int8_t;
-typedef __int32 int32_t;
-typedef unsigned __int8 uint8_t;
-typedef unsigned __int32 uint32_t;
-#else
 #include <stdint.h>
-#endif
 
 #include "image.h"
 #include "misc.h"
